22d06ce7ae9ca25e670940da9559d407367b21e1
success899 <success899@naver.com>
2022-11-02 01:08:17 +0900

    Create LordMore

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,33 +2,33 @@ import React, {useEffect, useState} from 'react';
 import {API_URL, API_KEY, IMAGE_URL} from '../../../Config';
 import MainImage from './Sections/MainImage';
 import GridCards from '../commons/GridCards';
-import axios from 'axios';
 import {Row} from 'antd';
 
 function LandingPage() {
 
   const [Movies, setMovies] = useState([])
   const [MainMovieImage, setMainMovieImage] = useState(null)
+  const [CurrentPage, setCurrentPage] = useState(0)
 
   useEffect(() => {
-
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint)
+}, [])
 
-    axios.get(endpoint)
-        .then(response => {
-            setMovies([...response.results])
-            setMainMovieImage(response.results[0])
-        })
-
+  const fetchMovies = (endpoint) => {
     fetch(endpoint)
         .then(response => response.json())
         .then(response => {                
-            setMovies([...response.results])
-            setMainMovieImage(response.results[0])
+            setMovies([...Movies, ...response.results])
+            setMainMovieImage(MainMovieImage || response.results[0])
+            setCurrentPage(response.page)
         })
+  }
 
-}, [])
-
+  const loadMoreMovies = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage+1}`;
+    fetchMovies(endpoint)
+  }
 
   return (
     <div style={{width: '100%', margin: '0'}}>
@@ -65,7 +65,7 @@ function LandingPage() {
       </div>
 
       <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button>Load More</button>
+        <button onClick={loadMoreMovies}>Load More</button>
       </div>
 
     </div>
