da5a5f6aa04812a8d2136dec35054f3562a07d5d
success899 <success899@naver.com>
2022-11-09 00:59:10 +0900

    GridCards Design fix

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -9,7 +9,7 @@ function GridCards(props) {
       <Col lg={6} md={8} xs={24}>
           <div style={{position: 'relative'}}>
               <a href={`/movie/${props.movieId}`}>
-                  <img style={{width: '100%', height: '320px'}} src={props.image} alt={props.movieName}/>
+                  <img style={{width: '100%', height: '450px'}} src={props.image} alt={props.movieName}/>
               </a>
           </div>
       </Col>
@@ -19,7 +19,7 @@ function GridCards(props) {
     return (
       <Col lg={6} md={8} xs={24}>
           <div style={{position: 'relative'}}>
-                <img style={{width: '100%', height: '320px'}} src={props.image} alt={props.CastName}/>
+                <img style={{width: '100%', height: '450px'}} src={props.image} alt={props.CastName}/>
           </div>
       </Col>
     )
